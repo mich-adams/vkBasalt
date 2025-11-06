485f6ee02e7f468550c432d1048451f2a383a85a
Bence Koczogh <bek30@hi.is>
2025-11-06 15:47:50 +0000

    Add depth sampler
    Use nearest filtering instead of linear

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -31,6 +31,35 @@ namespace vkBasalt
         return sampler;
     }
 
+    VkSampler createDepthSampler(LogicalDevice* pLogicalDevice)
+    {
+        VkSampler sampler;
+
+        VkSamplerCreateInfo samplerCreateInfo;
+        samplerCreateInfo.sType                   = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        samplerCreateInfo.pNext                   = nullptr;
+        samplerCreateInfo.flags                   = 0;
+        samplerCreateInfo.magFilter               = VK_FILTER_NEAREST;
+        samplerCreateInfo.minFilter               = VK_FILTER_NEAREST;
+        samplerCreateInfo.mipmapMode              = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        samplerCreateInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerCreateInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerCreateInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerCreateInfo.mipLodBias              = 0.0f;
+        samplerCreateInfo.anisotropyEnable        = VK_FALSE;
+        samplerCreateInfo.maxAnisotropy           = 16;
+        samplerCreateInfo.compareEnable           = VK_FALSE;
+        samplerCreateInfo.compareOp               = VK_COMPARE_OP_ALWAYS;
+        samplerCreateInfo.minLod                  = 0.0f;
+        samplerCreateInfo.maxLod                  = 0.0f;
+        samplerCreateInfo.borderColor             = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+        samplerCreateInfo.unnormalizedCoordinates = VK_FALSE;
+
+        VkResult result = pLogicalDevice->vkd.CreateSampler(pLogicalDevice->device, &samplerCreateInfo, nullptr, &sampler);
+        ASSERT_VULKAN(result);
+        return sampler;
+    }
+
     VkSampler createReshadeSampler(LogicalDevice* pLogicalDevice, const reshadefx::sampler_info& samplerInfo)
     {
         VkSampler sampler;
